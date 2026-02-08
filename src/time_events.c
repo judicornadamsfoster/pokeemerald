11ca1d1927032a2343f4bc62b7cf93c24ae206c1
judicornadamsfoster <judicornadamsfoster@gmail.com>
2026-02-08 22:16:06 +0000

    1
    add 5 npcs in littleroot town for debugging
    change options text speed to fast
    mirage island appears with a set flag

--- src/time_events.c
+++ src/time_events.c
@@ -41,14 +41,12 @@ void UpdateMirageRnd(u16 days)
 
 bool8 IsMirageIslandPresent(void)
 {
-    u16 rnd = GetMirageRnd() >> 16;
-    int i;
-
-    for (i = 0; i < PARTY_SIZE; i++)
-        if (GetMonData(&gPlayerParty[i], MON_DATA_SPECIES) && (GetMonData(&gPlayerParty[i], MON_DATA_PERSONALITY) & 0xFFFF) == rnd)
-            return TRUE;
+    if (FlagGet(FLAG_FORCE_MIRAGE_ISLAND))
+        return TRUE;
+		
 
     return FALSE;
+		
 }
 
 void UpdateShoalTideFlag(void)
