c24896f46a037c35259aa505257d8497553dbe6a
judicornadamsfoster <judicornadamsfoster@gmail.com>
2026-02-13 19:38:40 +0000

    press b to stop learning move

--- src/party_menu.c
+++ src/party_menu.c
@@ -4918,6 +4918,9 @@ static void Task_HandleStopLearningMoveYesNoInput(u8 taskId)
 
     switch (Menu_ProcessInputNoWrapClearOnChoose())
     {
+	case MENU_B_PRESSED:
+    PlaySE(SE_SELECT);
+    // fallthrough
     case 0:
         GetMonNickname(mon, gStringVar1);
         StringCopy(gStringVar2, gMoveNames[gPartyMenu.data1]);
@@ -4934,9 +4937,6 @@ static void Task_HandleStopLearningMoveYesNoInput(u8 taskId)
             gTasks[taskId].func = Task_ClosePartyMenuAfterText;
         }
         break;
-    case MENU_B_PRESSED:
-        PlaySE(SE_SELECT);
-        // fallthrough
     case 1:
         GetMonNickname(mon, gStringVar1);
         StringCopy(gStringVar2, gMoveNames[gPartyMenu.data1]);
